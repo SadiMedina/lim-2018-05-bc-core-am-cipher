3888d66eb607243468242b8054008a511de36a42
SadiMedina <sadi.medina.chavez@gmail.com>
2018-05-31 13:00:19 -0500

    Subiendo Modificacion en Funcion Encode-Decode/añadiendo estylos en CSS

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,30 @@
 window.cipher = {
-  // ... 
-};
+  encode:(offset, string)=>{
+    let posicion = parseInt(offset); //convertiendo en entero el desplazamiento
+    let palabraCifrada= " "; //para concatenar
+    for(let i=0;i<string.length;i++){
+       let codigoAscii=string.charCodeAt(i);
+       if(codigoAscii >= 65 && codigoAscii <=90){ 
+          palabraCifrada+=String.fromCharCode((codigoAscii-65+posicion)%26+65);}
+       else if(codigoAscii >=97 && codigoAscii <=122){
+              palabraCifrada+=String.fromCharCode((codigoAscii-97+posicion)%26+97);}
+       else {palabraCifrada+=String.fromCharCode(codigoAscii);}
+          }
+          return palabraCifrada;
+          },
+
+  decode:(offset, string)=>{
+     let posicion = parseInt(offset);
+     let palabraDescifrada = " " ; 
+     for(let i=0;i<string.length;i++){
+        let codigoAscii=string.charCodeAt(i);
+        if(codigoAscii >= 65 && codigoAscii <=90){
+          palabraDescifrada+=String.fromCharCode((codigoAscii-90-posicion)%26+90);}
+        else if(codigoAscii >=97 && codigoAscii<=122){
+                palabraDescifrada+=String.fromCharCode((codigoAscii-122-posicion)%26+122);}
+        else{palabraDescifrada+=String.fromCharCode(codigoAscii)}
+         }
+         return palabraDescifrada;
+         },
+         
+    }
--- src/index.js
+++ src/index.js
@@ -1,51 +1,18 @@
 
-/*Función Encriptar: Te permite ingresar un Texto en Mayusculas, Minuscula ... */
-function encriptar () {
-    let textoIngresado = document.getElementById("text-trabajo").value;
-    let posicion = parseInt(document.getElementById("desplazamiento").value);
-    let palabraCifrada = "";
-    
-    for (i = 0; i < textoIngresado.length; i++){
-      let CodigoAscii = textoIngresado.charCodeAt(i);
-      if(CodigoAscii >= 65 && CodigoAscii <=90 ){
-        palabraCifrada += String.fromCharCode((CodigoAscii-65+posicion)%26+65);
-        document.getElementById("text-resultado").value= palabraCifrada;}
-      
-      else if(CodigoAscii >= 97 && CodigoAscii <=122 ){
-        palabraCifrada += String.fromCharCode((CodigoAscii-97+posicion)%26+97);
-        document.getElementById("text-resultado").value= palabraCifrada;}
-      
-      else{
-        palabraCifrada += String.fromCharCode(CodigoAscii);
-        document.getElementById("text-resultado").value= palabraCifrada;} 
-      }
-      }
+let offset = document.getElementById("desplazamiento");
+let string = document.getElementById("text-trabajo");
+let clear  = document.getElementById("borrar");
 
-/*Función Desencriptar*/
-function desencriptar () {
-    let textoDescifrado = document.getElementById("text-trabajo").value;
-    let posicion = parseInt(document.getElementById("desplazamiento").value);
-    let palabraDescifrada = "";
-    
-    for (i = 0; i < textoDescifrado.length; i++){
-      let CodigoAscii = textoDescifrado.charCodeAt(i);
-      if(CodigoAscii >= 65 && CodigoAscii <=90 ){
-        palabraDescifrada += String.fromCharCode((CodigoAscii-65-posicion)%26);
-        document.getElementById("text-resultado").value= palabraDescifrada;}
-      
-      else if(CodigoAscii >= 97 && CodigoAscii <=122 ){
-        palabraDescifrada += String.fromCharCode((CodigoAscii-97-posicion)%26+97);
-        document.getElementById("text-resultado").value= palabraDescifrada;}
-      
-      else{
-        palabraDescifrada += String.fromCharCode(CodigoAscii);
-        document.getElementById("text-resultado").value= palabraDescifrada;} 
-      }
-      }
+document.getElementById("codificar").addEventListener("click",function(){              
+document.getElementById("text-resultado").value = window.cipher.encode(offset.value,string.value);
+})
 
-   
+document.getElementById("decodificar").addEventListener("click",function(){
+document.getElementById("text-resultado").value = window.cipher.decode(offset.value,string.value);
+})
 
-/*Funcion borrar*/
- function borrar(){
-  document.getElementById("text-trabajo").value="";
-}
+document.getElementById("borrar").addEventListener("click",function(){
+document.getElementById("text-trabajo").value = " ";
+})
+
+
